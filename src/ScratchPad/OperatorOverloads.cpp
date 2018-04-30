c97fe9f0abfe52f83d90b4d2b801a5513091ae13
Nathan V. Roberts <nvrober@sandia.gov>
2018-04-30 17:18:03 -0600

    Added operator overloads for nonlinear operations on Var, LinearTerm.

---
+++ src/ScratchPad/OperatorOverloads.cpp
@@ -0,0 +1,8 @@
+//
+//  OperatorOverloads.cpp
+//  Camellia
+//
+//  Created by Roberts, Nathan V on 4/30/18.
+//
+
+#include "OperatorOverloads.hpp"
---
+++ src/include/OperatorOverloads.hpp
@@ -0,0 +1,13 @@
+//
+//  OperatorOverloads.hpp
+//  Camellia
+//
+//  Created by Roberts, Nathan V on 4/30/18.
+//
+
+#ifndef OperatorOverloads_hpp
+#define OperatorOverloads_hpp
+
+#include <stdio.h>
+
+#endif /* OperatorOverloads_hpp */
